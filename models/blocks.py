23411fea934871c38c1510b17db0a7343a95be6f
Alex Meterez <alexandrumeterez@gmail.com>
2019-12-01 17:27:58 +0200

    finished blocks

--- models/blocks.py
+++ models/blocks.py
@@ -1,4 +1,42 @@
 import torch.nn as nn
+import torch.nn.functional as F
+import torch
+
+
+# TODO: UNDERSTAND!!!!!!
+class LocationAttention(nn.Module):
+    """
+    Calculates context vector based on previous decoder hidden state (query vector),
+    encoder output features, and convolutional features extracted from previous attention weights.
+    Attention-Based Models for Speech Recognition
+    https://arxiv.org/pdf/1506.07503.pdf
+    """
+
+    def __init__(self, encoded_dim, query_dim, attention_dim, num_location_features=32):
+        super(LocationAttention, self).__init__()
+        self.f = nn.Conv1d(in_channels=1, out_channels=num_location_features,
+                           kernel_size=31, padding=15, bias=False)
+        self.U = nn.Linear(num_location_features, attention_dim)
+        self.W = nn.Linear(query_dim, attention_dim)
+        self.V = nn.Linear(encoded_dim, attention_dim)
+        self.w = nn.Linear(attention_dim, 1, bias=False)
+        self.tanh = nn.Tanh()
+
+    def score(self, query_vector, encoder_out, mask):
+        encoder_out = self.V(encoder_out)  # (seq, batch, atten_dim) # project to attn dim
+        query_vector = self.W(query_vector)  # (seq, batch, atten_dim)
+        attention_energies = encoder_out + query_vector
+        location_features = self.f(mask.permute(1, 0, 2))  # (batch, 1, seq1_len)
+        attention_energies += self.U(location_features.permute(2, 0, 1))  # (seq, batch, numfeats)
+        return self.w(self.tanh(attention_energies))
+
+    def forward(self, query_vector, encoder_out, mask):
+        energies = self.score(query_vector, encoder_out, mask)
+        mask = F.softmax(energies, dim=0)
+        context = encoder_out.permute(1, 2, 0) @ mask.permute(1, 0, 2)  # (batch, seq1, seq2)
+        context = context.permute(2, 0, 1)  # (seq2, batch, encoder_dim)
+        mask = mask.permute(2, 1, 0)  # (seq2, batch, seq1)
+        return context, mask
 
 
 class ConvBlock(nn.Module):
@@ -94,6 +132,14 @@ class Encoder(nn.Module):
         self.conv1 = ConvBlock(in_channels=vocab_size, out_channels=vocab_size, kernel_size=5, padding=2)
         self.conv2 = ConvBlock(in_channels=vocab_size, out_channels=vocab_size, kernel_size=5, padding=2)
         self.conv3 = ConvBlock(in_channels=vocab_size, out_channels=vocab_size, kernel_size=5, padding=2)
+
+        """
+            From the paper:
+                The output of the final convolutional layer is passed into a
+                single bi-directional [19] LSTM [20] layer containing 512 units (256
+                in each direction) to generate the encoded features.
+        """
+
         self.rnn = nn.LSTM(input_size=vocab_size, hidden_size=256, bidirectional=True, dropout=0.1)
 
     def forward(self, input):
@@ -129,3 +175,65 @@ class Encoder(nn.Module):
 
         return output
 
+
+class Decoder(nn.Module):
+    """
+        The decoder is an autoregressive recurrent neural network which
+        predicts a mel spectrogram from the encoded input sequence one
+        frame at a time.
+    """
+
+    def __init__(self):
+        super().__init__()
+        """
+        The prediction from the previous time step is first
+        passed through a small pre-net containing 2 fully connected layers
+        of 256 hidden ReLU units. 
+        """
+        self.prenet = PreNet(in_channels=80, out_channels=256)
+        """
+        The encoder output is consumed by an attention network which
+        summarizes the full encoded sequence as a fixed-length context vector
+        for each decoder output step.
+        """
+        self.attention = LocationAttention(encoded_dim=256, query_dim=1024, attention_dim=128)
+        """
+        The prenet output and attention context vector are concatenated and passed
+        through a stack of 2 uni-directional LSTM layers with 1024 units.
+        """
+        # x2 because of the concatenation
+        self.rnn = nn.LSTM(input_size=256 * 2, hidden_size=1024, num_layers=2, dropout=0.1)
+        """
+        The concatenation of the LSTM output and the attention context
+        vector is projected through a linear transform to predict the target
+        spectrogram frame.
+        """
+        self.spec_out = nn.Linear(in_features=1024 + 256, out_features=80)
+        """
+        Finally, the predicted mel spectrogram is passed
+        through a 5-layer convolutional post-net which predicts a residual
+        to add to the prediction to improve the overall reconstruction
+        """
+        self.postnet = PostNet()
+        """
+        In parallel to spectrogram frame prediction, the concatenation of
+        decoder LSTM output and the attention context is projected down
+        to a scalar and passed through a sigmoid activation to predict the
+        probability that the output sequence has completed.
+        """
+        self.stop_out = nn.Linear(in_features=1024 + 256, out_features=1)
+
+    def forward(self, previous_out, encoder_out, decoder_hidden=None, mask=None):
+        """
+        Decodes a single frame
+        """
+        previous_out = self.prenet(previous_out)  # (4, 1, 256)
+        hidden, cell = decoder_hidden
+        context, mask = self.attention(hidden[:-1], encoder_out, mask)
+        rnn_input = torch.cat([previous_out, context], dim=2)
+        rnn_out, decoder_hidden = self.rnn(rnn_input, decoder_hidden)
+        spec_frame = self.spec_out(torch.cat([rnn_out, context], dim=2))  # predict next audio frame
+        stop_token = self.stop_out(torch.cat([rnn_out, context], dim=2))  # predict stop token
+        spec_frame = spec_frame.permute(1, 0, 2)
+        spec_frame = spec_frame + self.postnet(spec_frame)  # add residual
+        return spec_frame.permute(1, 0, 2), stop_token, decoder_hidden, mask
